94a2da8fb88baabeac80376502d09a514eb9f584
Ronaldo-Martinez <irmv_0311@hotmail.com>
2023-08-07 23:05:22 -0600

    Página de inicio servicel

--- resources/views/pages/base.blade.php
+++ resources/views/pages/base.blade.php
@@ -12,50 +12,118 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-    <nav class="bg-nav navbar  p-0 fixed-top">
-        <div class="container">
-            <a class="navbar-brand p-0" href="/">
-                <img src="/logo2.webp" alt="Logo servicel" class="logo">
-            </a>
-            <div class="d-none d-lg-block ">
-                <div class="nav d-flex">
-                    <a class="nav-link {{ request()->routeIs('inicio') ? 'active' : '' }}" aria-current="page" href="#">INICIO</a>
-                    <a class="nav-link {{ request()->routeIs('nosotros') ? 'active' : '' }}" href="#">NOSOTROS</a>
-                    <a class="nav-link {{ request()->routeIs('alquiler') ? 'active' : '' }}" href="#">ALQUILER</a>
-                    <a class="nav-link {{ request()->routeIs('contacto') ? 'active' : '' }}" href="#">CONTACTO</a>
+        <nav class="bg-nav navbar  p-0 fixed-top">
+            <div class="container">
+                <a class="navbar-brand p-0" href="/">
+                    <img src="/logo2.webp" alt="Logo servicel" class="logo">
+                </a>
+                <div class="d-none d-lg-block ">
+                    <div class="nav d-flex">
+                        <a class="nav-link {{ request()->routeIs('inicio') ? 'active' : '' }}" aria-current="page" href="#">INICIO</a>
+                        <a class="nav-link {{ request()->routeIs('nosotros') ? 'active' : '' }}" href="#">NOSOTROS</a>
+                        <a class="nav-link {{ request()->routeIs('alquiler') ? 'active' : '' }}" href="#">ALQUILER</a>
+                        <a class="nav-link {{ request()->routeIs('contacto') ? 'active' : '' }}" href="#">CONTACTO</a>
+                    </div>
                 </div>
-            </div>
 
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end text-dark bg-nav-movil" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <div class="text-warning text-center" id="offcanvasDarkNavbarLabel"></div>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body justify-content-center align-items-center d-flex">
-                    <ul class=" flex-grow-1 pe-3">
-                    <a class="nav-link {{ request()->routeIs('inicio') ? 'active' : 'text-white' }}" aria-current="page" href="#">INICIO</a>
-                    <a class="nav-link {{ request()->routeIs('nosotros') ? 'active' : 'text-white' }}" href="#">NOSOTROS</a>
-                    <a class="nav-link {{ request()->routeIs('alquiler') ? 'active' : 'text-white' }}" href="#">ALQUILER</a>
-                    <a class="nav-link {{ request()->routeIs('contacto') ? 'active' : 'text-white' }}" href="#">CONTACTO</a>
-                    </ul>
-                    
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end text-dark bg-nav-movil" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <div class="text-warning text-center" id="offcanvasDarkNavbarLabel"></div>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body justify-content-center align-items-center d-flex">
+                        <ul class=" flex-grow-1 pe-3">
+                        <a class="nav-link {{ request()->routeIs('inicio') ? 'active' : 'text-white' }}" aria-current="page" href="#">INICIO</a>
+                        <a class="nav-link {{ request()->routeIs('nosotros') ? 'active' : 'text-white' }}" href="#">NOSOTROS</a>
+                        <a class="nav-link {{ request()->routeIs('alquiler') ? 'active' : 'text-white' }}" href="#">ALQUILER</a>
+                        <a class="nav-link {{ request()->routeIs('contacto') ? 'active' : 'text-white' }}" href="#">CONTACTO</a>
+                        </ul>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
         <main>
             @yield('content')
         </main>
+
+        <footer class="bg-secondary">
+            <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-4 justify-content-center align-items-center d-flex">
+                    <div class=" my-4">
+                        <ul class="pe-3">
+                        <h2 class="nav-link justify-content-center text-white fs-5 py-3 text-center">Enlaces</h2>
+                        <a class="nav-link justify-content-center {{ request()->routeIs('inicio') ? 'active-footer' : 'text-white' }}" aria-current="page" href="#">INICIO</a>
+                        <a class="nav-link justify-content-center {{ request()->routeIs('nosotros') ? 'active-footer' : 'text-white' }}" href="#">NOSOTROS</a>
+                        <a class="nav-link justify-content-center {{ request()->routeIs('alquiler') ? 'active-footer' : 'text-white' }}" href="#">ALQUILER</a>
+                        <a class="nav-link justify-content-center {{ request()->routeIs('contacto') ? 'active-footer' : 'text-white' }}" href="#">CONTACTO</a>
+                        </ul>         
+                    </div>
+                </div>
+                <!--Información de contacto-->
+                <div class="col-12 col-lg-4 my-4 border-start border-end border-warning  text-white text-center">
+                    <div class="border-top border-warning d-block d-sm-none">
+                    </div>
+                    <h2 class="nav-link justify-content-center  text-white fs-5 py-3">Contáctanos</h2>
+                    <a class="d-flex align-items-center justify-content-center nav-link text-white" href="tel:+50322882451"> 
+                        <span class="material-symbols-outlined text-warning">
+                            call
+                        </span>+ 503 2288 - 2451
+                    </a>
+                    <a class="d-flex align-items-center justify-content-center nav-link text-white" href="tel:+50254674528"> 
+                        <span class="material-symbols-outlined text-warning">
+                            call
+                        </span>+ 502 5467 - 4528
+                    </a>
+                    <h2 class="text-white fs-5 py-3 fw-bold">Horarios de oficinas administrativas</h2>
+                    <div class="text-white text-center fs-5">
+                        <p class="m-0">Lunes a Viernes</p>
+                        <p>7:30 A.M. a 4:30 P.M.</p>
+                        <p class="m-0">Sábados</p>
+                        <p>7:30 A.M. a 12:00 M</p>
+                        <p class="fs-5 fw-bold">Brindamos soporte técnico las 24 horas</p>
+                    </div>
+                    <div class="border-bottom border-warning d-block d-sm-none">
+                    </div>
+                </div>
+                <!--Redes Sociales-->
+                <div class="col-12 col-lg-4 align-items-center justify-content-center d-flex">
+                    <div class="my-4">
+                        <h2 class="nav-link  text-white fs-5 py-3 text-center">Síguenos en nuestras redes sociales</h2>
+                        <div class="d-flex justify-content-center">
+                            <a href="">
+                                <img class="icon" src="/img/fb.webp" alt="Facebook icon">
+                            </a>
+                            <a href="">
+                                <img class="icon" src="/img/instagram.webp" alt="instagram icon">
+                            </a>
+                            <a href="">
+                                <img class="icon" src="/img/whatsapp.webp" alt="whatsapp Icon">
+                            </a>
+                        </div>
+                        <div class="align-items-center justify-content-center d-flex pt-2">
+                            <img class="img-logo d-flex justify-content-end" src="/logo2.webp" alt="Logo servicel">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center text-white">
+                    <p class="fs-4 m-2">© {{ date('Y') }} Todos los derechos reservados | SERVICEL S.A de C.V. </p>
+                </div>
+            </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
--- resources/views/pages/inicio.blade.php
+++ resources/views/pages/inicio.blade.php
@@ -105,7 +105,7 @@
                     <div class="card-img-overlay-custom">
                         <div class="container row">
                             <div class="col-6 bg-white d-grid gap-2 p-0">
-                                <a class="btn btn-outline-secondary rounded-0 fs-4" href="#">Excavadoras</a>
+                                <a class="btn btn-outline-secondary rounded-0 fs-4" href="#">Montacargas</a>
                             </div>
                         </div>
                     </div>
@@ -116,7 +116,7 @@
                     <div class="card-img-overlay-custom">
                         <div class="container row">
                             <div class="col-6 bg-white d-grid gap-2 p-0">
-                                <a class="btn btn-outline-secondary rounded-0 fs-4" href="#">Excavadoras</a>
+                                <a class="btn btn-outline-secondary rounded-0 fs-4" href="#">Motoniveladoras</a>
                             </div>
                         </div>
                     </div>
@@ -141,6 +141,48 @@
             <img src="/logo2.webp" alt="Logo servicel">
         </div>
     </article>
+</div>
+<!--Sección Ubicaciones-->
+<div>
+    <div class="bg-secondary text-white text-center p-3 my-5 px-5">
+        <h2 class="fw-bold">NUESTRAS UBICACIONES GEOGRÁFICAS</h2>
+    </div>
+    <div class="container-fluid my-3">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <h3 class="fw-bold text-warning bg-secondary fs-4 text-center">El Salvador</h3>
+                <div class="map-container">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d969.2171858308552!2d-89.28018473048105!3d13.665744202309032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDM5JzU2LjciTiA4OcKwMTYnNDYuNCJX!5e0!3m2!1ses!2ssv!4v1691469682252!5m2!1ses!2ssv" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="d-flex align-items-center justify-content-center pt-3 fs-5 fw-bold text-secondary" href="tel:+50322882451"> 
+                    <span class="material-symbols-outlined text-secondary">
+                        location_on
+                    </span>Santa Tecla, La Libertad
+                </div>
+                <a class="d-flex align-items-center justify-content-center pt-3 fs-5 fw-bold text-secondary text-decoration-none" href="tel:+50322882451"> 
+                    <span class="material-symbols-outlined text-secondary">
+                        call
+                    </span>+ 503 2288 - 2451
+                </a>
+            </div>
+            <div class="col-12 col-lg-6">
+                <h3 class="fw-bold text-warning bg-secondary fs-4 text-center">Guatemala</h3>
+                <div class="map-container">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1933.7350366547232!2d-90.73635650632048!3d14.225778619049937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8588fcdd5acf3b99%3A0x42843213332f485b!2sParque%20Industrial%20Tecnopark!5e0!3m2!1ses!2ssv!4v1691471069036!5m2!1ses!2ssv" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="d-flex align-items-center justify-content-center pt-3 fs-5 fw-bold text-secondary" href="tel:+50322882451"> 
+                    <span class="material-symbols-outlined text-secondary">
+                        location_on
+                    </span>Escuintla, Guatemala.
+                </div>
+                <a class="d-flex align-items-center justify-content-center pt-3 fs-5 fw-bold text-secondary text-decoration-none" href="tel:+50254674528"> 
+                    <span class="material-symbols-outlined text-secondary">
+                        call
+                    </span>+ 502 5467 - 4528
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
